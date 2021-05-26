d86bad56a8b17f0311134fc4e3b823530ac675c3
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-05-26 11:08:11 +0600

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+import NavBar from './NavBar'
+
+const Home = () => {
+  return (
+    <div>
+      <NavBar />
+      <Header />
+    </div>
+  )
+}
+
+export default Home
